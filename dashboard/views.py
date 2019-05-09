8b82a950e0d14fc4aefe603bfd7db3aa24da3402
Felix Knoepke <morytox@hotmail.de>
2019-05-09 10:34:08 +0200

    removes all mqtt inclusions from project. Adds requests to project and directs post information as json post to api server

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,52 +4,37 @@ import json
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from distutils.util import strtobool
-import paho.mqtt.client as mqtt
+import requests
 from .forms import CarControlForm
 
-broker = os.getenv('MQTT_BROKER', 'ts.rdy.one')
-port =  int(os.getenv('MQTT_PORT', 11883))
-pub_name = os.getenv('HOSTNAME', ('publisher-' + uuid.uuid4().hex.upper()[0:6]))
-websocket = strtobool(os.getenv('MQTT_SOCKET', 'False'))
-wait_timer = int(os.getenv('MQTT_WAITTIME', 1))
+api = os.getenv('API_SERVER', 'http://localhost:5000')
 
 def dashboard(request):
     if request.method == 'POST':
         form = CarControlForm(request.POST)
         if form.is_valid():
 
-            broker = os.getenv('MQTT_BROKER', 'ts.rdy.one')
-            port =  int(os.getenv('MQTT_PORT', 11883))
-            pub_name = os.getenv('HOSTNAME', ('publisher-' + uuid.uuid4().hex.upper()[0:6]))
-            websocket = strtobool(os.getenv('MQTT_SOCKET', 'False'))
-            wait_timer = int(os.getenv('MQTT_WAITTIME', 1))
-
-            if websocket:
-                messages.info(request, f'Connecting to {broker}:{port} as {pub_name} via websocket')
-                client = mqtt.Client(pub_name,transport='websockets')
-            else:
-                messages.info(request, f'Connecting to {broker}:{port} as {pub_name}')
-                client = mqtt.Client(pub_name)
-
-            client.connect(broker, port, 60)
-            client.loop_start()
-
             speed = form.cleaned_data.get('speed')
             angle = form.cleaned_data.get('angle')
 
             message = {
-                    'counter': 0,
                     'speed': 0,
                     'steering': 0,
-                    'hostname': pub_name,
                     }
             
             message['speed'] = speed
             message['steering'] = angle
-            client.publish('test/host', json.dumps(message))
+
+            #publish messages
+            url = f'{api}/api/v1/publish/message'
+            payload = message
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data=json.dumps(payload), headers=headers)
             messages.success(request, f'Instructions Transmitted. Speed: {speed}%, Steeringangle: {angle}%')
-            client.disconnect()
-            messages.info(request, f'Disconnected from Server.')
+            messages.info(request, f'Message Posted to {url}')
+            messages.info(request, f'Request Response: {r}')
+
+
             return redirect('dashboard')
     else: 
         form = CarControlForm()
